2aad0a6a5c821b57518d3d4dfb1f49f76944d99f
= <dasgoodase@gmail.com>
2021-05-21 21:49:02 +0400

    Create project

---
+++ src/webpack.config.js
@@ -0,0 +1,14 @@
+let path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './js/script.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    watch: true,
+    devtool: 'source-map',
+
+    module: {}
+};
